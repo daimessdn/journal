a0d382ffcd26a5485287c72c1385fc67f0f0ee34
daimessdn <daimessdn@gmail.com>
2020-04-27 00:51:07 +0700

    added toogler sidebar...

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     hljs.highlightBlock(block);
   });
 });
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "30%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
