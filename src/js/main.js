63236e0bef992ab1a993c083ad617539fab6bfbd
daimessdn <daimessdn@gmail.com>
2020-04-26 16:19:09 +0700

    added random python materials on my notes...

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// init'd highlight.js and begin highlighting all code related...
 document.addEventListener('DOMContentLoaded', (event) => {
   document.querySelectorAll('pre code').forEach((block) => {
     hljs.highlightBlock(block);
